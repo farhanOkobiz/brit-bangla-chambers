356cf3929d2892d26b9cd1774304f7d0d6964938
Kabir <kabir.okobiz@gmail.com>
2025-07-22 13:53:18 +0600

    commited missed one

--- server/src/routes/api/specializationRoutes.js
+++ server/src/routes/api/specializationRoutes.js
@@ -10,10 +10,10 @@ import upload from "../../middleware/multerMiddleware.js";
 
 const router = Router();
 
-router.post("/create-category", upload.single("image"), createSpecialization);
-router.get("/get-all-categories", getAllSpecializations);
-router.get("/get-category/:id", getSpecializationById);
-router.put("/update-category/:id", upload.single("image"), updateSpecialization);
-router.delete("/delete-category/:id", deleteSpecialization);
+router.post("/create-specialization", upload.single("image"), createSpecialization);
+router.get("/get-all-specialization", getAllSpecializations);
+router.get("/get-specialization/:id", getSpecializationById);
+router.put("/update-specialization/:id", upload.single("image"), updateSpecialization);
+router.delete("/delete-specialization/:id", deleteSpecialization);
 
 export default router;
