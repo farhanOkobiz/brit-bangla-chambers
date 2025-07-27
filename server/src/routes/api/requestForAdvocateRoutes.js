bdc9d78fadadc466a9a574b1ed603ea5196cf375
shohid-okobiz <dev.okobiz@gmail.com>
2025-07-27 10:58:08 +0600

    ok

--- server/src/routes/api/requestForAdvocateRoutes.js
+++ server/src/routes/api/requestForAdvocateRoutes.js
@@ -3,8 +3,6 @@ import {
   createAdvocateMessage,
   deleteAdvocateMessage,
   getAdvocateMessage,
-  acceptedMessageStatus,
-  rejectedMessageStatus,
   getRequestForAdvocate,
 } from "../../controllers/requestForAdvocateController.js";
 import { checkAdvocate } from "../../middleware/authMiddleware.js";
@@ -14,8 +12,6 @@ const router = Router();
 router.post("/", createAdvocateMessage);
 router.get("/", getAdvocateMessage);
 router.get("/advocate", checkAdvocate, getRequestForAdvocate);
-router.patch("/accepted/:id", checkAdvocate, acceptedMessageStatus);
-router.patch("/rejected/:id", checkAdvocate, rejectedMessageStatus);
 router.delete("/:id", deleteAdvocateMessage);
 
 export default router;
