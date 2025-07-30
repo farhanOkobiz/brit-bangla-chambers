419a1a126268046448798a823f7e65fb6fe1346f
Kabir <kabir.okobiz@gmail.com>
2025-07-30 15:11:05 +0600

    pulled

--- admin/pages/my_cases/DetailsFile.jsx
+++ admin/pages/my_cases/DetailsFile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { useAxios } from "../../services/useAxios";
+import { UseAxios } from "../../services/UseAxios";
 import {
   FaCalendarAlt,
   FaEdit,
@@ -32,7 +32,7 @@ function DetailsFile() {
   useEffect(() => {
     const fetchCase = async () => {
       try {
-        const res = await useAxios(`/showOwnCaseFile/singleCaseFile/${id}`);
+        const res = await UseAxios(`/showOwnCaseFile/singleCaseFile/${id}`);
         const data = res.data?.data;
         setFile(data);
       } catch {
