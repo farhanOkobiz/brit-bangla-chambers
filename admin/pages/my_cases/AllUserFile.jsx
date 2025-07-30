2315ffcb7e0afe69f1525ca0f0ff8393f7e8775a
mamon <mamon.okobiz@gmail.com>
2025-07-30 16:36:02 +0600

    nwe

--- admin/pages/my_cases/AllUserFile.jsx
+++ admin/pages/my_cases/AllUserFile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAxios } from "../../services/useAxios";
+import { UseAxios } from "../../services/UseAxios";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import { toast } from "react-toastify";
@@ -27,7 +27,7 @@ function AllUserFile() {
   useEffect(() => {
     const fetchCaseFiles = async () => {
       try {
-        const res = await useAxios("/showOwnCaseFile/allCaseFile");
+        const res = await UseAxios("/showOwnCaseFile/allCaseFile");
         setCaseFiles(res.data?.data || []);
         console.log("caseFile:", caseFiles);
       } catch (err) {
@@ -55,12 +55,11 @@ function AllUserFile() {
 
     if (result.isConfirmed) {
       // Optimistic update
-      const previousCaseFiles = [...caseFiles];
 
       setCaseFiles(caseFiles.filter((file) => file._id !== id));
 
       try {
-        await useAxios(`/showOwnCaseFile/deleteCaseFile/${id}`, {
+        await UseAxios(`/showOwnCaseFile/deleteCaseFile/${id}`, {
           method: "DELETE",
         });
 
@@ -74,7 +73,7 @@ function AllUserFile() {
           showConfirmButton: false,
         });
       } catch (error) {
-        setCaseFiles(previousCaseFiles);
+        console.error("Error deleting case file:", error);
         toast.error("Failed to delete the case file.");
       }
     }
@@ -218,7 +217,6 @@ function AllUserFile() {
                     to={`/advocate/dashboard/request-file/${file._id}`}
                     className="inline-flex items-center gap-2 px-4 py-2 text-sm font-semibold text-white bg-[#3BB77E] rounded-lg shadow-md hover:bg-[#319b69] transition duration-200"
                   >
-                    
                     Request Files
                   </Link>
 
